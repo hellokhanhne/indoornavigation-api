c3ba6a4b94c5d6352e3b69da3c040521f8a00540
nguyen ngoc khanh <84921391+hellokhanhne@users.noreply.github.com>
2023-06-27 13:31:41 +0700

    cm

--- index.js
+++ index.js
@@ -6,17 +6,15 @@ const fs = require("fs");
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  return res.json("ok");
+  return res.json("Webcome API endpoint");
 });
 
-app.get("/treenodes", (req, res) => {
-  if (!fs.existsSync("treenodes.json")) {
+app.get("/records", (req, res) => {
+  if (!fs.existsSync("records.json")) {
     return res.status(200).json([]);
   }
-  // Read the file contents as a string
-  const jsonString = fs.readFileSync("treenodes.json", "utf-8");
 
-  // Parse the JSON string into a JavaScript object
+  const jsonString = fs.readFileSync("records.json", "utf-8");
   const items = JSON.parse(jsonString);
   res.status(200).json(items);
 });
@@ -25,39 +23,31 @@ app.get("/nodes", (req, res) => {
   if (!fs.existsSync("nodes.json")) {
     return res.status(200).json([]);
   }
-  // Read the file contents as a string
-  const jsonString = fs.readFileSync("nodes.json", "utf-8");
 
-  // Parse the JSON string into a JavaScript object
+  const jsonString = fs.readFileSync("nodes.json", "utf-8");
   const items = JSON.parse(jsonString);
   res.status(200).json(items);
 });
 
-app.post("/treenodes", (req, res) => {
-  // Convert the array to a JSON string
+app.post("/records", (req, res) => {
   const json = JSON.stringify(req.body);
 
-  // Check if the file exists, and create it if it does not
-  if (!fs.existsSync("treenodes.json")) {
-    fs.writeFileSync("treenodes.json", "");
+  if (!fs.existsSync("records.json")) {
+    fs.writeFileSync("records.json", "");
   }
 
-  // Write the JSON string to the file
-  fs.writeFileSync("treenodes.json", json);
+  fs.writeFileSync("records.json", json);
   res.status(200).json("update success");
 });
 
 app.post("/nodes", (req, res) => {
-  // Convert the array to a JSON string
   console.log(req.body);
   const json = JSON.stringify(req.body);
 
-  // Check if the file exists, and create it if it does not
   if (!fs.existsSync("nodes.json")) {
     fs.writeFileSync("nodes.json", "");
   }
 
-  // Write the JSON string to the file
   fs.writeFileSync("nodes.json", json);
   res.status(200).json("update success");
 });
